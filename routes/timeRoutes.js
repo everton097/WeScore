df24d915ed6d064e95c19b52aa6f45dbcc8a5a2a
Everton F Silva <everton097v1@outlook.com>
2024-02-28 20:29:55 -0300

    Correção/remoção de arquivos antigos.

--- routes/timeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const TimeController = require('../controllers/timeController')
-
-router.get('/time',TimeController.criarTime)
-router.post('/time',TimeController.criarTimePost)
-
-module.exports = router
--- routes/usuariosRoutes.js
+++
@@ -1,9 +0,0 @@
-/* const express = require('express')
-const router = express.Router()
-const UsuarioController = require('../controllers/usuarioController')
-
-router.get('/usuarios',UsuarioController.criarUsuario)
-router.post('/usuarios',UsuarioController.criarUsuarioPost)
-
-module.exports = router
- */
