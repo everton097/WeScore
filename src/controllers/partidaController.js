61fd217a1eebba014d0e6f3b87dd494070d09758
Everton F Silva <everton097v1@outlook.com>
2024-08-06 21:50:22 -0300

    Remoção de logs.

--- src/controllers/partidaController.js
+++ src/controllers/partidaController.js
@@ -11,7 +11,6 @@ exports.getPartidaCampeonatoById = async (req,res) =>{
         const config = {
             headers: { Authorization: `Bearer ${token}` }
         }
-
         // Fazer a requisição para a API
         let response = await api.get(`/partida/${idPartida}`, config)
         const partida = response.data
@@ -20,10 +19,6 @@ exports.getPartidaCampeonatoById = async (req,res) =>{
         const jogadoresTime1 = response.data
         response = await api.get(`/time/players/${partida.idTime2}`, config)
         const jogadoresTime2 = response.data
-        
-        console.log("Partidas",partida)
-        console.log("JogadoresTime1",jogadoresTime1)
-        console.log("JogadoresTime2",jogadoresTime2)
         res.render('jogos', { jsonPartida: JSON.stringify(partida), idPartida: partida.idPartida , jogadoresTime1: JSON.stringify(jogadoresTime1), jogadoresTime2: JSON.stringify(jogadoresTime2), user, spaUrl, apiUrl, layout : 'painelws' })
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar datas.' });
